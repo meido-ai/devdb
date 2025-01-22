dbee291716d09ce26ade73d99e6c12e0a9965e89
John Craft <jcraft@hey.com>
2025-01-22 09:30:17 -0800

    Update Jest to use ES modules

--- api/jest.config.cjs
+++ api/jest.config.cjs
@@ -1,8 +1,17 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
-  preset: 'ts-jest',
+  preset: 'ts-jest/presets/default-esm',
   testEnvironment: 'node',
   roots: ['<rootDir>/src'],
   testMatch: ['**/*.test.ts'],
   moduleFileExtensions: ['ts', 'js', 'json', 'node'],
+  extensionsToTreatAsEsm: ['.ts'],
+  transform: {
+    '^.+\\.tsx?$': [
+      'ts-jest',
+      {
+        useESM: true,
+      },
+    ],
+  },
 };
